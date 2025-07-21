8f4cadca7c15ad036a65c960c4778240d73c6c8c
kishu01karb <166857376+kishu01karb@users.noreply.github.com>
2025-07-21 19:25:42 +0530

    Add files via upload

---
+++ M3_Task_1_check_if_a_number_is_even_or_odd.py
@@ -0,0 +1,10 @@
+#Task 1: Check if a Number is Even or Odd
+#Problem Statement:  Write a Python program that:
+#1. 	Takes an integer input from the user.
+number = int(input("Enter the number: "))
+#2. 	Checks whether the number is even or odd using an if-else statement.
+if number%2==0:
+    print(number," is an Even Number")
+else:
+    print(number," is an Odd Number")
+#3. 	Displays the result accordingly.
---
+++ M3_Task_2_sum_of_integers_from_1_to_50_using_a_loop.py
@@ -0,0 +1,9 @@
+#Task 2: Sum of Integers from 1 to 50 Using a Loop
+#Problem Statement: Write a Python program that:
+#1.Uses a for loop to iterate over numbers from 1 to 50.
+SUM = 0
+for num in range(1,51):
+#2. Calculates the sum of all integers in this range.
+    SUM+=num
+#3.Displays the final sum.
+print("The sum of numbers from 1 to 50 is : ", SUM)
